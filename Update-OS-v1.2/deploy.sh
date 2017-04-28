d172ee0a662aefb8225d0e09a2177bbc5c0bd0de
Owen Osborn <owen@critterandguitari.com>
2017-04-27 23:16:29 -0400

    what

---
+++ Update-OS-v1.2/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Updating OS"
+
+# remount root read write
+/root/scripts/remount-rw.sh
+
+# copy files
+cp -f /usbdrive/Patches/Update-OS-v1.2/root/mother.pd /root
+cp -f /usbdrive/Patches/Update-OS-v1.2/root/mother /root
+cp /usbdrive/Patches/Update-OS-v1.2/root/.bash_profile /root
+cp /usbdrive/Patches/Update-OS-v1.2/root/.jwmrc /root
+cp /usbdrive/Patches/Update-OS-v1.2/root/.pdsettings /root
+cp /usbdrive/Patches/Update-OS-v1.2/root/scripts/* /root/scripts
+
+# sync
+sync 
+
+# just chill
+sleep 1
+
+# normally we'd want to remount read only, but this is not possible because of cp -f
+# so we'll just shutdown instead
+#/root/scripts/shutdown.sh
