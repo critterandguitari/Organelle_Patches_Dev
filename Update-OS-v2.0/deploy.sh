b134d1e547ce6701c2dbdb86d4155a162e799c35
Owen Osborn <owen@critterandguitari.com>
2017-05-09 10:50:50 -0400

    adding 2.0 updater and example patches

---
+++ Update-OS-v2.0/deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "Updating OS"
+
+# remount root read write
+/root/scripts/remount-rw.sh
+
+# copy files
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/mother.pd /root
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/mother /root
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/.bash_profile /root
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/.jwmrc /root
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/.pdsettings /root
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/version /root
+cp -f /usbdrive/Patches/Update-OS-v2.0/root/scripts/* /root/scripts
+
+# sync
+sync 
+
+# just chill
+sleep 1
+
+# normally we'd want to remount read only, but this is not possible because of cp -f
+# but pd patch will call shutdown after this anyway
