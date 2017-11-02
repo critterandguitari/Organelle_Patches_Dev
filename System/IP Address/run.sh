02177cdf2f50dd17ec8654c151666a9380d2106c
Owen Osborn <owen@critterandguitari.com>
2017-11-02 10:10:43 -0400

    host to ip

--- System/IP Address/run.sh
+++ System/IP Address/run.sh
@@ -4,10 +4,11 @@
 # on first line before this script is called
 
 IP=$(ip -o -4 addr list wlan0 | awk '{print $4}' | cut -d/ -f1)
+HOST=$(ps aux | grep 'avahi.*running' | awk 'NR==1{print $13}' | sed 's/\[//' | sed 's/\]//')
 
-oscsend localhost 4001 /oled/aux/line/1 s " "
-oscsend localhost 4001 /oled/aux/line/2 s "IP Address:"
-oscsend localhost 4001 /oled/aux/line/3 s "$IP"
+oscsend localhost 4001 /oled/aux/line/1 s "$IP"
+oscsend localhost 4001 /oled/aux/line/2 s "$HOST"
+oscsend localhost 4001 /oled/aux/line/3 s " "
 oscsend localhost 4001 /oled/aux/line/4 s " "
 oscsend localhost 4001 /oled/aux/line/5 s " "
 
